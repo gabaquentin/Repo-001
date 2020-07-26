cadf829d4c30a6de49f5fc408970ac5ca9454d1b
arthking17 <48186621+arthking17@users.noreply.github.com>
2020-07-26 18:56:08 +0100

    ajout des pages de détail de catégorie, de détail de service, de la liste des services du côté front-end

--- src/Controller/eservices/ServicesFrontController.php
+++ src/Controller/eservices/ServicesFrontController.php
@@ -8,10 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class ServicesFrontController extends AbstractController
 {
     /**
-     * @Route("/services/accueil",name="services_accueil")
+     * @Route("/front/services/accueil",name="services_accueil")
      */
     public function accueil_services()
     {
         return $this->render("frontend/eservices/accueil.html.twig");
+    }
+
+    /**
+     * @Route("/front/services/categorie",name="categorie")
+     */
+    public function detail_categorie()
+    {
+        return $this->render("frontend/eservices/categorie.html.twig");
+    }
+
+    /**
+     * @Route("/front/services/categorie/services",name="services")
+     */
+    public function liste_services_par_categorie()
+    {
+        return $this->render("frontend/eservices/services.html.twig");
+    }
+
+    /**
+     * @Route("/front/services/categorie/servicex",name="service")
+     */
+    public function afficher_service()
+    {
+        return $this->render("frontend/eservices/service.html.twig");
     }
 }
